e1a818e0c3596f522087927483c9d83062f0f2d6
Anatolii Litynskiy <anatolii.litynskiy@voypost.com>
2022-07-25 14:49:40 +0300

    init app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Button>asdqwe</Button>
+    </div>
+  );
+}
+
+export default App;
